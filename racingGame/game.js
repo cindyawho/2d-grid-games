79cb99094251159e72bb16e599b0f2ecc561549e
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-06-02 14:54:55 -0700

    Change bricks to track, delete paddle instances, use array for grid and track creation

--- racingGame/game.js
+++ racingGame/game.js
@@ -8,18 +8,26 @@ var ballY = 75;
 var ballSpeedX = 5;
 var ballSpeedY = 7;
 
-const BRICK_W = 80;
-const BRICK_H = 20;
-const BRICK_GAP = 2;
-const BRICK_COLS = 10;
-const BRICK_ROWS = 14;
-var brickGrid = new Array(BRICK_COLS * BRICK_ROWS);
-var bricksLeft = 0;
-
-const PADDLE_WIDTH = 100;
-const PADDLE_THICKNESS = 10;
-const PADDLE_DIST_FROM_EDGE = 60;
-var paddleX = 400;
+const TRACK_W = 40;
+const TRACK_H = 40;
+const TRACK_GAP = 2;
+const TRACK_COLS = 20;
+const TRACK_ROWS = 15;
+var trackGrid = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
+                 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1,
+                 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1,
+                 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1,
+                 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1,
+                 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1,
+                 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1,
+                 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1,
+                 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1,
+                 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1,
+                 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1,
+                 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1,
+                 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
 
 var mouseX, mouseY;
 
@@ -31,27 +39,12 @@ function updateMousePos(evt) {
     mouseX = evt.clientX - rect.left - root.scrollLeft;
     mouseY = evt.clientY - rect.top - root.scrollTop;
     
-    paddleX = mouseX - PADDLE_WIDTH/2;
-
     //cheat / hack to test ball in any position
     // ballX = mouseX;
     // ballY = mouseY;
     // ballSpeedX = 4;
     // ballSpeedY = -4;
 }
-
-// ~~~~~~~~~~~~~~~~ Handle Brick Population ~~~~~~~~~~~~~~~~
-function brickReset() {
-    bricksLeft = 0;
-    var i;
-    for(i = 0; i<3*BRICK_COLS;i++){
-        brickGrid[i] = false;
-    }
-    for(; i< BRICK_COLS * BRICK_ROWS; i++) {
-        brickGrid[i] = true;
-        bricksLeft++;
-    } // end of for loop
-} // end of brickReset func
 
 // ~~~~~~~~~~~~~~~~ Main Game Code ~~~~~~~~~~~~~~~~
 window.onload = function() {
@@ -63,7 +56,6 @@ window.onload = function() {
     //move paddle with mouse movement
     canvas.addEventListener("mousemove", updateMousePos);
 
-    brickReset();
     ballReset();
 }
 
@@ -92,50 +84,42 @@ function ballMove(){
     }
     if(ballY > canvas.height) { // bottom
         ballReset();
-        brickReset();
     } 
 }
 
-function isBrickAtColRow(col, row) {
-    if(col >=0 && col < BRICK_COLS && 
-        row >= 0 && row < BRICK_ROWS) {
+function isTrackAtColRow(col, row) {
+    if(col >=0 && col < TRACK_COLS && 
+        row >= 0 && row < TRACK_ROWS) {
         
-        var brickIndexUnderCord = rowColtoArrayIndex(col, row);
-        return brickGrid[brickIndexUnderCord];
+        var trackIndexUnderCord = rowColtoArrayIndex(col, row);
+        return (trackGrid[trackIndexUnderCord] == 1);
     } else {
         return false;
     }
 }
 
-function ballBrickHandling() {
-    var ballBrickCol = Math.floor(ballX/ BRICK_W);
-    var ballBrickRow = Math.floor(ballY/BRICK_H);
-    var brickIndexUnderBall = rowColtoArrayIndex(ballBrickCol, ballBrickRow);
-    if(ballBrickCol>=0 && ballBrickCol < BRICK_COLS && 
-        ballBrickRow >= 0 && ballBrickRow < BRICK_ROWS){
+function ballTrackHandling() {
+    var ballTrackCol = Math.floor(ballX/ TRACK_W);
+    var ballTrackRow = Math.floor(ballY/TRACK_H);
+    var trackIndexUnderBall = rowColtoArrayIndex(ballTrackCol, ballTrackRow);
+    if(ballTrackCol>=0 && ballTrackCol < TRACK_COLS && 
+        ballTrackRow >= 0 && ballTrackRow < TRACK_ROWS){
     
-        if(isBrickAtColRow(ballBrickCol, ballBrickRow)){
-            brickGrid[brickIndexUnderBall] = false;
-            bricksLeft--;
-            // console.log(bricksLeft);
-            if(bricksLeft == 0){
-
-            }
-
+        if(isTrackAtColRow(ballTrackCol, ballTrackRow)){
             var prevBallX = ballX - ballSpeedX;
             var prevBallY = ballY - ballSpeedY;
-            var prevBrickCol = Math.floor(prevBallX / BRICK_W);
-            var prevBrickRow = Math.floor(prevBallY / BRICK_H);
+            var prevTrackCol = Math.floor(prevBallX / TRACK_W);
+            var prevTrackRow = Math.floor(prevBallY / TRACK_H);
 
             var bothTestsFailed = true;
-            if(prevBrickCol != ballBrickCol) {
-                if(isBrickAtColRow(prevBrickCol, ballBrickRow)==false){
+            if(prevTrackCol != ballTrackCol) {
+                if(isTrackAtColRow(prevTrackCol, ballTrackRow)==false){
                     ballSpeedX *= -1;
                     bothTestsFailed = false;
                 }
             }
-            if(prevBrickRow != ballBrickRow) {
-                if(isBrickAtColRow(prevBrickCol, ballBrickRow)==false){
+            if(prevTrackRow != ballTrackRow) {
+                if(isTrackAtColRow(prevTrackCol, ballTrackRow)==false){
                     ballSpeedY *= -1;
                     bothTestsFailed = false;
                 }
@@ -145,76 +129,39 @@ function ballBrickHandling() {
                 ballSpeedX *= -1;
                 ballSpeedY *= -1;
             }
-        } // end of brick gounf
-    } // end of brick row and col
-} // end of ballBrickHandling func
-
-function ballPaddleHandling(){
-    var paddleTopEdgeY = canvas.height-PADDLE_DIST_FROM_EDGE;
-    var paddleBottomEdgeY = paddleTopEdgeY + PADDLE_THICKNESS;
-    var paddleLeftEdgeX = paddleX;
-    var paddleRightEdgeX = paddleLeftEdgeX + PADDLE_WIDTH;
-    
-    // paddle collision logic
-    if(ballY >= paddleTopEdgeY && //below top
-        ballY <= paddleBottomEdgeY && //above bottom
-        ballX >= paddleLeftEdgeX && //right of left edge
-        ballX <= paddleRightEdgeX) { //left of right edge
-            
-            ballSpeedY *= -1;
-            
-            var centerOfPaddleX = paddleX + PADDLE_WIDTH/2;
-            var ballDistFromPaddleCenterX = ballX - centerOfPaddleX;
-            ballSpeedX = ballDistFromPaddleCenterX * 0.35;
-
-            if(bricksLeft == 0) {
-                brickReset();
-            } //out of bricks
-    }//ball center inside paddle
-}// end of ballPaddleHandling
+        } // end of track gounf
+    } // end of track row and col
+} // end of ballTrackHandling func
 
 function moveAll() {
     ballMove();
     
-    ballBrickHandling();
-
-    ballPaddleHandling();
+    ballTrackHandling();
 }
 
 function rowColtoArrayIndex(col, row) {
-    return col + BRICK_COLS * row;
+    return col + TRACK_COLS * row;
 }
 
-function drawBricks() {
-    for(var eachRow = 0; eachRow < BRICK_ROWS; eachRow++){
-        for(var eachCol = 0; eachCol < BRICK_COLS; eachCol++){
+function drawTracks() {
+    for(var eachRow = 0; eachRow < TRACK_ROWS; eachRow++){
+        for(var eachCol = 0; eachCol < TRACK_COLS; eachCol++){
             
             var arrayIndex = rowColtoArrayIndex(eachCol, eachRow);
             
-            if(brickGrid[arrayIndex]) {
-                colorRect(BRICK_W*eachCol, BRICK_H*eachRow, BRICK_W-BRICK_GAP,BRICK_H - BRICK_GAP, "blue");
-            } // end of is this brick here
-        } //end of for each brick        
+            if(trackGrid[arrayIndex] == 1) {
+                colorRect(TRACK_W*eachCol, TRACK_H*eachRow, TRACK_W-TRACK_GAP,TRACK_H - TRACK_GAP, "blue");
+            } // end of is this track here
+        } //end of for each track        
     }
-} // end of drawBricks
+} // end of drawTracks
 
 function drawAll() {
     colorRect(0, 0, canvas.width, canvas.height, "black"); //clear screen
     
     colorCircle(ballX, ballY, 10, "white"); //draw ball
 
-    colorRect(paddleX, canvas.height - PADDLE_DIST_FROM_EDGE, PADDLE_WIDTH, PADDLE_THICKNESS, "white");
-
-    drawBricks();
-
-    
-    // colorText(mouseBrickCol+","+mouseBrickRow+":"+brickIndexUnderMouse, mouseX,mouseY, "yellow");
-
-    //showing mouse pointer to disappear bricks
-    // if(brickIndexUnderMouse>=0 && brickIndexUnderMouse < BRICK_COLS*BRICK_ROWS){
-    //     brickGrid[brickIndexUnderMouse] = false;
-    // }
-
+    drawTracks();
 }
 
 function colorRect(topLeftX, topLeftY, boxWidth, boxHeight, fillColor){
