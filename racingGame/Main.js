26de0a28583bf216d32325ec4650838201c2d022
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-06-04 16:55:03 -0700

    Restructure files to organize code and improve readability

---
+++ racingGame/Main.js
@@ -0,0 +1,40 @@
+console.log("Welcome to the racing game!");
+
+// ~~~~~~~~~~~~~~~~ Global Variables ~~~~~~~~~~~~~~~~
+var canvas, canvasContext;
+var framesPerSecond = 30;
+
+
+// ~~~~~~~~~~~~~~~~ Main Game Code ~~~~~~~~~~~~~~~~
+window.onload = function() {
+    canvas = document.getElementById('gameCanvas');
+    canvasContext = canvas.getContext("2d");
+
+    setInterval(updateAll, 1000/framesPerSecond);
+
+    setUpInput();
+
+    carImageLoad();
+
+    carReset();
+}
+
+function updateAll() {
+    moveAll();
+    drawAll();
+}
+
+function moveAll() {
+    carMove();   
+    carTrackHandling();
+}
+
+function clearScreen() {
+    colorRect(0, 0, canvas.width, canvas.height, "black"); //clear screen
+}
+
+function drawAll() {
+    clearScreen();
+    carDraw();
+    drawTracks();
+}
