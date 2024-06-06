67f92fc44f26659c448bf469a84cc89a18d774a9
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-06-05 19:42:38 -0700

    Create loading screen to wait for images to load
    yes it's only a frame or few frames for the screen locally but just in case

--- racingGame/Main.js
+++ racingGame/Main.js
@@ -9,6 +9,9 @@ var framesPerSecond = 30;
 window.onload = function() {
     canvas = document.getElementById('gameCanvas');
     canvasContext = canvas.getContext("2d");
+    
+    colorRect(0, 0, canvas.width, canvas.height, "black");
+    colorText("LOADING IMAGES...", canvas.width/2, canvas.height/2, "white")
 
     loadImages();
 }
