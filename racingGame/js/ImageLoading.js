79aa7df0d043ef3c29b650755bf0e12520515d69
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-06-05 20:40:13 -0700

    Create and load new images & art

--- racingGame/js/ImageLoading.js
+++ racingGame/js/ImageLoading.js
@@ -1,12 +1,15 @@
 var carPic = document.createElement("img");
 var roadPic = document.createElement("img");
 var wallPic = document.createElement("img");
+var goalPic = document.createElement("img");
+var treePic = document.createElement("img");
+var flagPic = document.createElement("img");
 
 var picsToLoad = 0; // set automatically based on imageList in loadImages();
 
 function countLoadedImagesAndLaunchIfReady(){
     picsToLoad--;
-    console.log(picsToLoad);
+    // console.log(picsToLoad);
     if(picsToLoad == 0){
         imageLoadingDoneSoStartGame();
     }
@@ -22,6 +25,9 @@ function loadImages() {
       {varName: carPic, theFile: "player1car.png"},
       {varName: roadPic, theFile: "track_road.png"},
       {varName: wallPic, theFile: "track_wall.png"},
+      {varName: goalPic, theFile: "goalFinish.png"},
+      {varName: treePic, theFile: "track_tree.png"},
+      {varName: flagPic, theFile: "track_flag.png"},
     ];
 
     picsToLoad = imageList.length;
