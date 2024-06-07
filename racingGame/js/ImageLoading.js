e90f1d30cb9218e1c49497f4d1b0acb098884a95
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-06-06 18:05:13 -0700

    Save all track tile art into an array

--- racingGame/js/ImageLoading.js
+++ racingGame/js/ImageLoading.js
@@ -1,4 +1,6 @@
 var carPic = document.createElement("img");
+
+var trackPics = [];
 var roadPic = document.createElement("img");
 var wallPic = document.createElement("img");
 var goalPic = document.createElement("img");
@@ -19,20 +21,29 @@ function beginLoadingImage(imgVar, fileName) {
     imgVar.onload = countLoadedImagesAndLaunchIfReady;
     imgVar.src = "./images/"+fileName;
 }
+function loadImageForTrackCode(trackCode, fileName){
+    trackPics[trackCode] = document.createElement("img");
+    beginLoadingImage(trackPics[trackCode], fileName);
+}
 
 function loadImages() {
     var imageList = [
       {varName: carPic, theFile: "player1car.png"},
-      {varName: roadPic, theFile: "track_road.png"},
-      {varName: wallPic, theFile: "track_wall.png"},
-      {varName: goalPic, theFile: "goalFinish.png"},
-      {varName: treePic, theFile: "track_tree.png"},
-      {varName: flagPic, theFile: "track_flag.png"},
+
+      {trackType: TRACK_ROAD, theFile: "track_road.png"},
+      {trackType: TRACK_WALL, theFile: "track_wall.png"},
+      {trackType: TRACK_GOAL, theFile: "goalFinish.png"},
+      {trackType: TRACK_TREE, theFile: "track_tree.png"},
+      {trackType: TRACK_FLAG, theFile: "track_flag.png"},
     ];
 
     picsToLoad = imageList.length;
 
     for(var i = 0; i < picsToLoad; i++){
-        beginLoadingImage(imageList[i].varName, imageList[i].theFile);
+        if(imageList[i].varName != undefined) {
+            beginLoadingImage(imageList[i].varName, imageList[i].theFile);
+        } else {
+            loadImageForTrackCode(imageList[i].trackType, imageList[i].theFile);
+        }
     }
 }
